860203776d9b72469967b70ea8d5c7680772cc5a
etiennegithub21 <96472371+etiennegithub21@users.noreply.github.com>
2023-01-04 17:25:28 +0100

    Update compile.sh

--- verifie/compile.sh
+++ verifie/compile.sh
@@ -4,5 +4,5 @@ ocamlc -c color_heap.ml &&
 ocamlc -c color_heap.cmo graphStructure.ml && 
 ocamlfind ocamlc -package yojson -linkpkg -g -c graphStructure.cmo instanceConverter.ml &&
 ocamlfind ocamlc -package yojson -linkpkg -g -o convert color_heap.cmo graphStructure.cmo instanceConverter.cmo convert.ml && 
-ocamlfind ocamlc -package yojson -linkpkg -g -o color color_heap.cmo graphStructure.cmo instanceConverter.cmo color.ml
+ocamlfind ocamlc -package yojson -linkpkg -g -o color color_heap.cmo graphStructure.cmo instanceConverter.cmo color.ml &&
 ocamlfind ocamlc -package yojson -linkpkg -g -o show color_heap.cmo graphStructure.cmo instanceConverter.cmo show.ml
